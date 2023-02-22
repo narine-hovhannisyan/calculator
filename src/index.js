7bb3f9400d66c2e6d66a9d4e69dc39d153fde9c1
narine-hovhannisyan <narine2233@gmail.com>
2023-02-22 17:52:39 +0400

    Calculator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {useState} from "react";
+import './App.css';
+import ReactDOM from "react-dom/client";
+ function Counter() {
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+     const[counter,setcounter]=useState(null);
+     const[count,setcount]=useState(null);
+     const [updated, setUpdated] = useState(counter);
+     const [changed, setchanged] = useState(count);
+     const [answer, setanswer] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+     const reset =() => {
+         setcounter(0)
+         setcount(0)
+         setUpdated(0)
+         setchanged(0)
+         setanswer(0)
+     }
+     const changefirstValue = (event) => {
+         setcounter(event.target.value);
+     }
+     const changesecondValue = (event) => {
+         setcount(event.target.value);
+     }
+     const firstClick = () => {
+         setUpdated(+counter);
+     };
+     const secondClick = () => {
+         setchanged(+count);
+     };
+     const sumNumbers = () => {
+         setanswer(updated + changed)
+     }
+     const minusNumbers = () => {
+         setanswer(updated - changed)
+     }
+     const bajanumNumbers = () => {
+         setanswer(updated * changed)
+     }
+     const bazmapaykumNumbers = () => {
+         setanswer(updated / changed)
+     }
+     return (
+         <div className="counter">
+             <h1>Calculator</h1>
+             {/*first input*/}
+             <input
+                 type = "number"
+                 name="counter"
+                 onChange={changefirstValue}
+                 value={counter}
+             />
+             {/*second input*/}
+             <input
+                 type = "number"
+                 name="counter"
+                 onChange={changesecondValue}
+                 value={count}
+             />
+             <h2>Number one: {updated} <br/> Number two: {changed}</h2>
+             <span className="counter__output">{answer}</span>
+             <div className="btn__container">
+                 <button className="control__btn"  onClick={firstClick}>get first number</button>
+                 <button className="control__btn"  onClick={secondClick}>get second number</button>
+                 <button className="control__btn"  onClick={sumNumbers}>+</button>
+                 <button className="control__btn"  onClick={minusNumbers}>-</button>
+                 <button className="control__btn"  onClick={bajanumNumbers}>*</button>
+                 <button className="control__btn"  onClick={bazmapaykumNumbers}>/</button>
+                 <button className="reset" onClick={reset}>Reset</button>
+             </div>
+         </div>
+     )
+ }
+ const root = ReactDOM.createRoot(document.getElementById('mydiv'));
+ root.render(<Counter/>);
+
+
